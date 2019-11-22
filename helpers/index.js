fd2138d3d580c39268cb3639ce4613f20996c51a
Roman <39159831+SydorRoman@users.noreply.github.com>
2019-11-22 20:40:50 +0200

    refactor

--- helpers/index.js
+++ helpers/index.js
@@ -1,3 +1,4 @@
 module.exports.tokenizer = require('./tokenizer');
 module.exports.tokenVereficator = require('./token-vereficator');
 module.exports.tokenVereficator = require('./sendEmail');
+// aslo this file usless
