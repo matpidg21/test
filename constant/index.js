7a9ac9b93e0e4f0ed10c320351e547800ad282e3
Roman <39159831+SydorRoman@users.noreply.github.com>
2019-11-22 20:32:37 +0200

    refactor

--- constant/index.js
+++ constant/index.js
@@ -2,3 +2,4 @@ module.exports.DB_TABLES = require('./db-table-enum');
 module.exports.JWT_SECRET = require('./jwt-secret');
 module.exports.MAIL_CREDENTIAL = require('./mail-credential');
 module.exports.AWS_SECRET = require('./aws-secret');
+ // remove this file/ You dont need it
