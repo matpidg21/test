98644446cd956e9c49ea5891420a287232a02e4b
Roman <39159831+SydorRoman@users.noreply.github.com>
2019-11-22 20:43:03 +0200

    refactor

--- helpers/tokenizer.js
+++ helpers/tokenizer.js
@@ -5,7 +5,7 @@ const {JWT_SECRET} = require('../constant');
 module.exports = () => {
     const access_token = jwt.sign({}, JWT_SECRET.ACCESS, {expiresIn: '24h'});
     const refresh_token = jwt.sign({}, JWT_SECRET.REFRESH, {expiresIn: '96h'});
-
+    // 24, 96 => config
 
     return {
         access_token,
